ecf2efc2003f8e48d0d870ae0aead066692460bb
chestnut90 <cowzon90@gmail.com>
2023-12-04 16:02:43 +0900

    Feature : add channels daphne
    - add chanells[daphne]
    - asgi setting

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter({"http": asgi_application})
