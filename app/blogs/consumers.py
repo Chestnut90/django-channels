74864db6fecc1fefd463fbcc0b95d43fa76f4386
chestnut90 <cowzon90@gmail.com>
2023-12-08 17:03:56 +0900

    remove: remove print statement in consumer

--- app/blogs/consumers.py
+++ app/blogs/consumers.py
@@ -11,13 +11,10 @@ class LivePostConsumer(WebsocketConsumer):
         self.send(json.dumps(event))
 
     def livepost_post_created(self, event: dict):
-        print("1receive, ", event)
         self._sender(event)
 
     def livepost_post_updated(self, event: dict):
-        print("1updated, ", event)
         self._sender(event)
 
     def livepost_post_deleted(self, event: dict):
-        print("d1eleted, ", event)
         self._sender(event)
