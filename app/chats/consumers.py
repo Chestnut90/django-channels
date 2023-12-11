e71a15c0de47366fdccf346a179e80e65fe08b0a
chestnut90 <cowzon90@gmail.com>
2023-12-11 14:20:19 +0900

    feature : add consumer for chat service

--- app/chats/consumers.py
+++ app/chats/consumers.py
@@ -1,6 +1,34 @@
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
 
 
 class EchoConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         self.send(f"Echo : {text_data}")
+
+
+class ChatConsumer(JsonWebsocketConsumer):
+
+    GROUP_NAME = "square"
+    groups = [GROUP_NAME]
+
+    def receive_json(self, content, **kwargs):
+        if "chats.message" != content["type"]:
+            raise Exception("invalid event type")
+
+        message = content["message"]
+        async_to_sync(self.channel_layer.group_send)(
+            self.GROUP_NAME,
+            {
+                "type": "chats.message",
+                "message": message,
+            },
+        )
+
+    def chats_message(self, messages):
+        self.send_json(
+            {
+                "type": "chats.message",
+                "message": messages["message"],
+            }
+        )
--- app/chats/routings.py
+++ app/chats/routings.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from chats.consumers import EchoConsumer
+from chats.consumers import EchoConsumer, ChatConsumer
 
 websocket_urlpatterns = [
     path("ws/echo/", EchoConsumer.as_asgi()),
+    path("ws/chats/<str:room_name>/", ChatConsumer.as_asgi()),
 ]
