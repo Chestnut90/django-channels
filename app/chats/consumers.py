0628b05c9911befe4abcecfa74883d9e23f4f62b
chestnut90 <cowzon90@gmail.com>
2023-12-11 14:36:07 +0900

    refact : separate group_name on consumer

--- app/chats/consumers.py
+++ app/chats/consumers.py
@@ -8,9 +8,28 @@ class EchoConsumer(WebsocketConsumer):
 
 
 class ChatConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group_name = ""
 
-    GROUP_NAME = "square"
-    groups = [GROUP_NAME]
+    def connect(self):
+        room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.group_name = f"chats-{room_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name,
+        )
+
+        self.accept()
+
+    def disconnect(self, code):
+
+        if self.group_name:
+            async_to_sync(self.channel_layer.group_discard)(
+                self.group_name,
+                self.channel_name,
+            )
 
     def receive_json(self, content, **kwargs):
         if "chats.message" != content["type"]:
@@ -18,7 +37,7 @@ class ChatConsumer(JsonWebsocketConsumer):
 
         message = content["message"]
         async_to_sync(self.channel_layer.group_send)(
-            self.GROUP_NAME,
+            self.group_name,
             {
                 "type": "chats.message",
                 "message": message,
