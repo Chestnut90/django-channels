f1a1e8597943bdae4c4192b171ae18f70ed77e0d
chestnut90 <cowzon90@gmail.com>
2023-12-11 10:56:16 +0900

    feature : add chat room html and views

--- app/chats/urls.py
+++ app/chats/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from chats import views
 
+app_name = "chats"
+
 urlpatterns = [
     path("echo/", views.echo_page),
+    path("", views.index, name="index"),
+    path("<str:room_name>/", views.room_chat, name="room_chat"),
 ]
--- app/chats/views.py
+++ app/chats/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 
 def echo_page(request):
     return render(request, "chats/echo_page.html")
+
+
+def index(request):
+    return render(request, "chats/index.html")
+
+
+def room_chat(request, room_name):
+    return render(
+        request,
+        "chats/room_chat.html",
+        {
+            "room_name": room_name,
+        },
+    )
