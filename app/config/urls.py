a013eb3ce54116ec0c3c32bc492302c9f44868f4
chestnut90 <cowzon90@gmail.com>
2023-12-12 15:34:32 +0900

    feature : home as chats index page by redirect

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("chats/", include("chats.urls")),
     path("blogs/", include("blogs.urls")),
     path("users/", include("users.urls")),
+    path("", RedirectView.as_view(pattern_name="chats:index"), name="root"),
 ]
